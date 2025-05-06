d14a90a66df0e1d072faee56a9211da605896108
nargok <nargok@gmail.com>
2025-05-06 12:39:01 +0900

    Home view

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,8 @@
 import type { RouteConfig } from '@react-router/dev/routes';
-import { route } from '@react-router/dev/routes';
+import { index, route } from '@react-router/dev/routes';
 
 export default [
+  index("routes/home.tsx"),
   route("contacts/:contactId", "routes/contact.tsx"),
 ] satisfies RouteConfig;
 
---
+++ app/routes/home.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <p id="inex-page">
+        This is the home page.
+        <br />
+        <a href="https://reactrouter.com" target="_blank">
+            Check out the docs at reactrouter.com
+        </a>
+    </p>
+  )
+}
